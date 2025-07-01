e00c504eae9522a76b60d21fa70b21cfd08bee79
Piyush Pandey <ppiyushkumar274@gmail.com>
2025-07-01 16:44:07 +0530

    Display fetched orders in Orders page

--- dashboard/src/components/Orders.js
+++ dashboard/src/components/Orders.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 
 const Orders = () => {
+    const [orders, setOrders] = useState([]);
+     useEffect(() => {
+    axios
+      .get("https://zerodha-clone-lb23.onrender.com/allOrders")
+      .then((res) => setOrders(res.data))
+      .catch((err) => console.error("Failed to fetch orders", err));
+  }, []);
   return (
-    <div className="orders">
+    /*<div className="orders">
       <div className="no-orders">
         <p>You haven't placed any orders today</p>
 
@@ -11,6 +20,42 @@ const Orders = () => {
           Get started
         </Link>
       </div>
+    </div> */
+     <div className="orders">
+      {orders.length === 0 ? (
+        <div className="no-orders">
+          <p>You haven't placed any orders today</p>
+          <Link to={"/"} className="btn">
+            Get started
+          </Link>
+        </div>
+      ) : (
+        <>
+          <h3 className="title">Your Orders ({orders.length})</h3>
+          <div className="order-table">
+            <table>
+              <thead>
+                <tr>
+                  <th>Stock</th>
+                  <th>Qty</th>
+                  <th>Price</th>
+                  <th>Mode</th>
+                </tr>
+              </thead>
+              <tbody>
+                {orders.map((order, index) => (
+                  <tr key={index}>
+                    <td>{order.name}</td>
+                    <td>{order.qty}</td>
+                    <td>{order.price}</td>
+                    <td>{order.mode}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </>
+      )}
     </div>
   );
 };
